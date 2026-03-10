4eb39c0d8babed1c6bcd05ae37bd719cef4a9e2e
bordeauxred <2robert.mueller@gmail.com>
2026-03-10 20:20:13 +0100

    feat: export EpisodeCollector, AsyncLearner, RewardPipeline, wrap from lfx

--- lfx/__init__.py
+++ lfx/__init__.py
@@ -3,14 +3,23 @@
 __version__ = "0.1.0"
 
 from lfx.agent import LfXAgent
+from lfx.collector import EpisodeCollector
 from lfx.core.env import EvalResult, Sample, StaticTaskEnvironment
+from lfx.core.reward import RewardPipeline, RewardSignal
+from lfx.learner import AsyncLearner
 from lfx.llm import LiteLLMClient, MockLLMClient
+from lfx.wrapper import wrap
 
 __all__ = [
+    "AsyncLearner",
+    "EpisodeCollector",
+    "EvalResult",
     "LfXAgent",
     "LiteLLMClient",
     "MockLLMClient",
-    "EvalResult",
+    "RewardPipeline",
+    "RewardSignal",
     "Sample",
     "StaticTaskEnvironment",
+    "wrap",
 ]
