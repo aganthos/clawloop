d22de71e15e30c20861bbeffcd336d650cef487e
bordeauxred <2robert.mueller@gmail.com>
2026-03-11 20:36:55 +0100

    feat: export CompletionResult, TokenLogProb, LfxCallback from lfx

--- lfx/__init__.py
+++ lfx/__init__.py
@@ -3,8 +3,11 @@
 __version__ = "0.1.0"
 
 from lfx.agent import LfXAgent
+from lfx.callbacks.litellm_cb import LfxCallback
 from lfx.collector import EpisodeCollector
+from lfx.completion import CompletionResult
 from lfx.core.env import EvalResult, Sample, StaticTaskEnvironment
+from lfx.core.episode import TokenLogProb
 from lfx.core.reward import RewardPipeline, RewardSignal
 from lfx.learner import AsyncLearner
 from lfx.llm import LiteLLMClient, MockLLMClient
@@ -12,14 +15,17 @@ from lfx.wrapper import wrap
 
 __all__ = [
     "AsyncLearner",
+    "CompletionResult",
     "EpisodeCollector",
     "EvalResult",
     "LfXAgent",
+    "LfxCallback",
     "LiteLLMClient",
     "MockLLMClient",
     "RewardPipeline",
     "RewardSignal",
     "Sample",
     "StaticTaskEnvironment",
+    "TokenLogProb",
     "wrap",
 ]
