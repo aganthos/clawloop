599584fc17532d0b689d2ea511e6e20954769f93
bordeauxred <2robert.mueller@gmail.com>
2026-03-16 16:09:49 +0100

    feat: add HarnessLearningBackend

--- lfx/backends/__init__.py
+++ lfx/backends/__init__.py
@@ -1,3 +1,4 @@
 from lfx.backends.base import BackendError, LfXBackend, SkyRLBackendInitError
+from lfx.backends.harness_learning import HarnessLearningBackend, HarnessLearningConfig
 
-__all__ = ["BackendError", "LfXBackend", "SkyRLBackendInitError"]
+__all__ = ["BackendError", "LfXBackend", "SkyRLBackendInitError", "HarnessLearningBackend", "HarnessLearningConfig"]
